48d4a912ef6672f12d6963b5fb58761264b41493
belinipek <90098325+belinipek@users.noreply.github.com>
2021-09-28 21:47:55 +0300

    Add files via upload

---
+++ code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs import String
+
+def publisher():
+    pub = rospy.Publisher("robotics_itu_first_topic",String,queue_size=10)
+    rospy.init_node("publisher_node",anonymous=True)
+    rate = rospy.Rate(10) 
+    while not rospy.is_shutdown():
+        msg = "our very first publisher code!"
+        rospy.loginfo(msg)
+        pub.publish(msg)
+        rate.sleep()
+if __name__ == "__main__":
+     publisher()
+     
